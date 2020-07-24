acb3a3243dfd040ea3ff3cab434f76077b4de499
lyj <challengelongyj@163.com>
2020-07-24 10:19:13 +0800

    将node_modules从远程仓库删除，并且保留本地仓库的该文件夹

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,23 +1,50 @@
 <template>
     <div id="firstDemo">
+      <ol>
+        <!--循环-->
+        <li v-for="nameVal in nameVals">
+          {{ nameVal.nameVal }}
+        </li>
+        <li v-for="(index, key, val) in nameVals">
+          {{ key }},{{val}},{{index}}
+        </li>
+
+        <!--if-else-->
+        <div v-if="Math.random() > 0.5">
+          ran大于0.5
+        </div>
+        <div v-else>
+          ran 小于等于0.5
+        </div>
+
+        <el-dialog></el-dialog>
+
+      </ol>
       <h1>{{demoDetail}}</h1>
       <h1>{{detail()}}</h1>
     </div>
 </template>
 
 <script>
-  let newVar = {
-    name: 'Demo',
-    data: {
-      demoDetail: 'demo详情'
-    },
-    methods: {
-      detail: function () {
-        return this.demoDetail
-      }
+let newVar = {
+  name: 'Demo',
+  data () {
+    return {
+      demoDetail: 'demo详情',
+      nameVals: [
+        {nameVal: 'lyj-1'},
+        {nameVal: 'lyj-2'},
+        {nameVal: 'lyj-3'}
+      ]
+    }
+  },
+  methods: {
+    detail () {
+      return this.demoDetail
     }
   }
-  export default newVar
+}
+export default newVar
 </script>
 
 <style scoped>
